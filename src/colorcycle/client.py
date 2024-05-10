ed50cd065e02de5cd915cc4a584ac5b3acf7dba5
Matt McCarthy <matt@mccarthycode.com>
2024-05-10 11:19:22 -0500

    Convert variable names & address TODOs in Client

--- src/colorcycle/client.py
+++ src/colorcycle/client.py
@@ -14,19 +14,23 @@ from src.color import RGB, HSL
 
 
 class Client:
-	""" TODO
+	""" Runtime logic definitions for terminal application
 	"""
 
 	def __init__(self):
-		""" TODO
+		""" Initialization method for client
 		"""
 		from signal import signal, SIGINT
 
-		signal(SIGINT, self.interrupt_handler)
+		# Cancel running on keyboard interrupt
+		signal(SIGINT, self.interruptHandler)
 
 
-	def create_image(self, pixel : RGB, filepath : Path = None) -> Path:
-		""" TODO
+	def createImage(self, pixel : RGB, filepath : Path = None) -> None:
+		""" Create a single-pixel image in PNG format
+
+		:param pixel:    The RGB values of the pixel to be used as a background
+		:param filepath: The complete path (including directory and filename) of the output file. If no filepath is specified, the location used is the '/tmp' directory and the image is named after its hex values with a PNG extention.
 		"""
 		from PIL import Image
 
@@ -36,11 +40,13 @@ class Client:
 		if not filepath.exists():
 			image.save(filepath)
 
-		return filepath
 
+	def setWallpaperDarwin(self, pixel : RGB) -> int:
+		""" Set the wallpaper via applescript
 
-	def set_wallpaper_darwin(self, pixel : RGB) -> int:
-		""" TODO
+		:param pixel:      The RGB values of the pixel to be used as a background
+
+		:return: The exit status of the `osascript` command
 		"""
 		from os import system
 
@@ -51,27 +57,27 @@ class Client:
 				end tell
 			end tell
 		'''.strip()
-		exit_status = system(f"osascript -e '{applescript}'")
 
-		return exit_status
+		return system(f"osascript -e '{applescript}'")
 
 
-	def update(self, pixel : RGB, last_pixel : RGB = None) -> RGB:
-		""" TODO
+	def update(self, pixel : RGB, lastPixel : RGB = None) -> None:
+		""" Create image if it does not exist, and set the wallpaper tio that image
+
+		:param pixel:     The RGB values of the pixel to be used as a background
+		:param lastPixel: The RGB values previously used (for skipping repeat logs)
 		"""
-		last_pixel = last_pixel or RGB(0, 0, 0)
+		lastPixel = lastPixel or RGB(0, 0, 0)
 
-		if f'{pixel:hex}' != f'{last_pixel:hex}':
+		if f'{pixel:hex}' != f'{lastPixel:hex}':
 			print(f'[{datetime.now()}] #{pixel:hex}')
-			self.create_image(pixel)
 
-		self.set_wallpaper_darwin(pixel)
-
-		return pixel
+		self.createImage(pixel)
+		self.setWallpaperDarwin(pixel)
 
 
-	def interrupt_handler(self, sig, frame):
-		""" TODO
+	def interruptHandler(self, sig, frame) -> None:
+		""" Handle a keyboard interrupt (^C) to break main program execution
 		"""
 		# Log on a new line
 		print('\nInterrupt signal received')
@@ -80,24 +86,25 @@ class Client:
 		self.running = False
 
 
-	def run(self):
-		""" TODO
+	def run(self) -> None:
+		""" Main program execution loop
 		"""
 		from time import sleep
 
 		from src.colorcycle import Time
 
-		previous_rgb : RGB  = None
+		previousRGB  : RGB  = None
 		self.running : bool = True
 
 		while self.running:
-			current_time : time = datetime.now().time()
-			time_obj     : Time = Time(current_time.hour, current_time.minute, current_time.second)
-			hsl          : HSL  = time_obj.toHSL()
-			rgb          : RGB  = hsl.toRGB()
+			currentTime : time = datetime.now().time()
+			timeObj     : Time = Time(currentTime.hour, currentTime.minute, currentTime.second)
+			hsl         : HSL  = timeObj.toHSL()
+			rgb         : RGB  = hsl.toRGB()
 
-			previous_rgb = self.update(rgb, previous_rgb)
+			self.update(rgb, previousRGB)
 
-			if self.running:
-				sleep(1)
+			previousRGB = rgb
+
+			sleep(1)
 
